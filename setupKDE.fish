531b74cc585f66f15c6373e2221a8acea4bcf6a6
George <shalva258@gmail.com>
2019-09-29 02:48:45 +0400

    Update setupKDE.fish

--- setupKDE.fish
+++ setupKDE.fish
@@ -1,7 +1,47 @@
 #!/usr/bin/fish
 
-
 #kwinrc
 kwriteconfig5 --file $HOME/.config/kwinrc --group Windows --key BorderlessMaximizedWindows true
+kwriteconfig5 --file $HOME/.config/kwinrc --group MouseBindings --key CommandAllKey Meta
+kwriteconfig5 --file $HOME/.config/kwinrc --group MouseBindings --key CommandWindow1 "Activate, raise and pass click"
+kwriteconfig5 --file $HOME/.config/kwinrc --group MouseBindings --key CommandWindow2 "Activate, raise and pass click"
+kwriteconfig5 --file $HOME/.config/kwinrc --group MouseBindings --key CommandWindow3 "Activate, raise and pass click"
+kwriteconfig5 --file $HOME/.config/kwinrc --group MouseBindings --key CommandWindowWheel "Scroll"
 
 #krunnerrc
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key CharacterRunnerEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key DictionaryEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key Kill RunnerEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key PowerDevilEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key Spell CheckerEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key baloosearchEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key bookmarksEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key browsertabsEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key calculatorEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key desktopsessionsEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key konsoleprofilesEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key krunner_appstreamEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key kwinEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key locationsEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key org.kde.activitiesEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key org.kde.datetimeEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key org.kde.windowedwidgetsEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key placesEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key plasma-desktopEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key recentdocumentsEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key servicesEnabled "true"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key shellEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key unitconverterEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key webshortcutsEnabled "false"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key windowsEnabled "false"
+
+#font
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key XftAntialias "true"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key XftHintStyle ""
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key XftSubPixel "rgb"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key fixed "Monospace,9,-1,5,50,0,0,0,0,0"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key font "Cantarell,10,-1,5,50,0,0,0,0,0,Regular"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key menuFont "Cantarell,10,-1,5,50,0,0,0,0,0,Regular"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key shadeSortColumn "true"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key smallestReadableFont "Cantarell,10,-1,5,50,0,0,0,0,0,Regular"
+kwriteconfig5 --file $HOME/.config/krunnerrc --group Plugins --key toolBarFont "Cantarell,10,-1,5,50,0,0,0,0,0,Regular"
