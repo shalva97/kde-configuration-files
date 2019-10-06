0d0733981bfdfe0dd25b86f4b18faddf08fdd4ff
George <shalva258@gmail.com>
2019-10-07 00:22:40 +0400

    Update set_up_everything_else.fish

--- set_up_everything_else.fish
+++ set_up_everything_else.fish
@@ -1,1 +1,5 @@
 
+#keyboard layout
+mkdir $HOME/.config/xkb
+curl https://raw.githubusercontent.com/shalva97/dotFiles/master/home/.config/xkb/kayboard%20layout > $HOME/.config/xkb/kl
+curl https://raw.githubusercontent.com/shalva97/dotFiles/master/home/scripts/set_xkb_layout.sh > $HOME/.config/autostart-scripts/set_keyboard_layout.sh
