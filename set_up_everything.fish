95c2442b547f50ae219a628c4f01eee4fad227e8
George <shalva258@gmail.com>
2019-11-08 17:26:12 +0400

    Update set_up_everything.fish

--- set_up_everything.fish
+++ set_up_everything.fish
@@ -4,3 +4,6 @@ mkdir $HOME/.config/xkb
 curl https://raw.githubusercontent.com/shalva97/dotFiles/master/home/.config/xkb/kayboard%20layout > $HOME/.config/xkb/kl
 curl https://raw.githubusercontent.com/shalva97/dotFiles/master/home/scripts/set_xkb_layout.sh > $HOME/.config/autostart-scripts/set_keyboard_layout.sh
 chmod +x $HOME/.config/autostart-scripts/set_keyboard_layout.sh
+
+curl https://raw.githubusercontent.com/shalva97/set-up-files/master/setupFish.fish | fish
+curl https://raw.githubusercontent.com/shalva97/set-up-files/master/setupKDE.fish | fish
