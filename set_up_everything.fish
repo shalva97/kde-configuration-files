5a992dad345dcefec8918bee4023bfb1e77306ac
George <shalva258@gmail.com>
2019-12-01 22:18:38 +0400

    Update set_up_everything.fish

--- set_up_everything.fish
+++ set_up_everything.fish
@@ -5,5 +5,7 @@ curl https://raw.githubusercontent.com/shalva97/dotFiles/master/home/.config/xkb
 curl https://raw.githubusercontent.com/shalva97/dotFiles/master/home/scripts/set_xkb_layout.sh > $HOME/.config/autostart-scripts/set_keyboard_layout.sh
 chmod +x $HOME/.config/autostart-scripts/set_keyboard_layout.sh
 
+#other small things
 curl https://raw.githubusercontent.com/shalva97/set-up-files/master/setupFish.fish | fish
 curl https://raw.githubusercontent.com/shalva97/set-up-files/master/setupKDE.fish | fish
+curl https://raw.githubusercontent.com/shalva97/dotFiles/master/home/.config/plasma-org.kde.plasma.desktop-appletsrc > $HOME/.config/plasma-org.kde.plasma.desktop-appletsrc
