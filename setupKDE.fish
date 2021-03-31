616825becb9f1ce0c6730d545c93cbf17d58358e
George <shalva258@gmail.com>
2021-03-31 12:03:56 +0400

    panel height
    probably not correct way but works...

--- setupKDE.fish
+++ setupKDE.fish
@@ -179,3 +179,8 @@ kwriteconfig5 --file $HOME/.config/breezerc --group "Windeco Exception 0" --key 
 kwriteconfig5 --file $HOME/.config/breezerc --group "Windeco Exception 0" --key "ExceptionType" "0"
 kwriteconfig5 --file $HOME/.config/breezerc --group "Windeco Exception 0" --key "HideTitleBar" "true"
 kwriteconfig5 --file $HOME/.config/breezerc --group "Windeco Exception 0" --key "Mask" "16"
+
+# change panel height
+set panelNum (cat $HOME/.config/plasmashellrc | string match --regex Panel\ [0-9] | string match --regex [0-9])[1]
+qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "panels()[$panelNum - 1].height = 30"
+
