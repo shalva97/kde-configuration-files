07003d920de489d6f4ab3572681695e4693b66ea
shalva97 <shalva258@gmail.com>
2021-04-30 14:52:56 +0400

    cleanup

--- scripts/setupKDE.fish
+++ scripts/setupKDE.fish
@@ -186,3 +186,26 @@ qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "panel
 
 # eenable user feedback because why not
 kwriteconfig5 --file $HOME/.config/kde.org/UserFeedback.org.kde.plasmashell.conf --group "UserFeedback" --key "ApplicationStartCount" "7"
+
+# panel settings
+# credit goes to Zren https://gist.github.com/Zren/764f17c26be4ea0e088f4a6a1871f528 give him a star!!
+
+set script plasmasetconfig.py
+
+if test ! -e $script
+    echo downloading script
+    curl "https://gist.githubusercontent.com/Zren/764f17c26be4ea0e088f4a6a1871f528/raw/fb61ba26bfc44b7ba83bd7450f218504b65858ca/plasmasetconfig.py" > $script
+    chmod +x $script
+end
+
+# disable burp sound when changing volume
+./plasmasetconfig.py org.kde.plasma.volume General volumeFeedback false
+# clock
+./plasmasetconfig.py org.kde.plasma.digitalclock Appearance showDate false
+
+# task manager stuff
+./plasmasetconfig.py org.kde.plasma.taskmanager General groupedTaskVisualization 3 # show list of windows when clicking groups
+./plasmasetconfig.py org.kde.plasma.taskmanager General middleClickAction Close
+./plasmasetconfig.py org.kde.plasma.taskmanager General separateLaunchers false
+./plasmasetconfig.py org.kde.plasma.taskmanager General wheelEnabled false
+
